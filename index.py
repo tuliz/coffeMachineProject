a52306287b29bca643b6866ca8dfa7c1c183bf43
yarden <yarden.tulchinsky@gmail.com>
2024-06-10 18:09:47 +0300

    adding a function for deducting the used resources for the drink

--- index.py
+++ index.py
@@ -24,24 +24,19 @@ MENU = {
     }
 }
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
 #varaibles initialization
 money = 0
 power = True
 
 #return the resources in print format
-def printReport():
+def printReport(resources):
     """return the report of resources in printable format."""
     return f'Water: {resources["water"]}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${money}\n'
 
 #sending the answer the customer chosen (if its latte/cappuchino/espresso) and checking if resources are sufficient for making this drink
-def checkResources(drinkType):
+def checkResources(drinkType, resources):
     """getting the drinkType from Menu the customer chosen and returning True or False if resources sufficient."""
+
     #looping trough MENU and finding the dring that was chosen
     for drink in MENU:
         if drink == drinkType:
@@ -57,20 +52,34 @@ def checkResources(drinkType):
                 print('Not enough coffee]\n')
                 return False
 
+#when there are enough resources for the drinks, getting the drinkType and resources and deducting the used resources for the drink           
+def useResources(drinktype, resources):
+    """getting the drink type and resources if sufficient resources and returning resources after deducting the used recources for the drink."""
+    for resource in resources:
+        resources[resource] -= MENU[drinktype]["ingredients"][resource]
+    return resources
+
 
 #coffe machine start
 def coffeMachine():
+    
+    resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+    
     while power:
         answer = input("What would you like? (espresso/latte/cappuccino):").lower()
         if answer == 'off':
             return
         elif answer == 'report':
-            print(printReport())
+            print(printReport(resources))
         elif answer == 'espresso' or answer == 'latte' or answer == 'cappuccino':
-            if not checkResources(drinkType=answer):
+            if not checkResources(drinkType=answer, resources=resources):
                 continue
             else:
-                print(f'{MENU["cappuccino"]['ingredients']['milk']}')
+                resources = useResources(drinktype=answer, resources=resources)
 
 
 coffeMachine()
