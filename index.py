600d54936b459c8120d981939d064985aa5edbd8
yarden <yarden.tulchinsky@gmail.com>
2024-06-10 17:37:59 +0300

    adding function for checking if resources are sufficient for making the drinks

--- index.py
+++ index.py
@@ -39,15 +39,38 @@ def printReport():
     """return the report of resources in printable format."""
     return f'Water: {resources["water"]}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${money}\n'
 
+#sending the answer the customer chosen (if its latte/cappuchino/espresso) and checking if resources are sufficient for making this drink
+def checkResources(drinkType):
+    """getting the drinkType from Menu the customer chosen and returning True or False if resources sufficient."""
+    #looping trough MENU and finding the dring that was chosen
+    for drink in MENU:
+        if drink == drinkType:
+            if resources["water"] >= MENU[drink]['ingredients']['water'] and resources["milk"] >= MENU[drink]['ingredients']['milk'] and resources["coffee"] >= MENU[drink]['ingredients']['coffee']:
+                return True
+            if resources["water"] < MENU[drink]['ingredients']['water']:
+                print('Not enough water]\n')
+                return False
+            if resources["milk"] < MENU[drink]['ingredients']['milk']:
+                print('Not enough milk]\n')
+                return False
+            if resources["milk"] < MENU[drink]['ingredients']['coffee']:
+                print('Not enough coffee]\n')
+                return False
+
+
 #coffe machine start
 def coffeMachine():
     while power:
-        answer = input("What would you like? (espresso/latte/cappuccino):")
+        answer = input("What would you like? (espresso/latte/cappuccino):").lower()
         if answer == 'off':
             return
         elif answer == 'report':
             print(printReport())
-
+        elif answer == 'espresso' or answer == 'latte' or answer == 'cappuccino':
+            if not checkResources(drinkType=answer):
+                continue
+            else:
+                print(f'{MENU["cappuccino"]['ingredients']['milk']}')
 
 
 coffeMachine()
